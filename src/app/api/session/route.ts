e1b74fc838f7b632f9755cbc668351f5097d952e
htetahyan <htetahyan@gmail.com>
2024-11-04 14:55:17 +0000

    feat(update link with skeleton and pricing

--- src/app/api/session/route.ts
+++ src/app/api/session/route.ts
@@ -7,14 +7,17 @@ import { NextRequest, NextResponse } from "next/server";
 
 export const POST=async(req:NextRequest)=>{
   try{  const {sessionId}=await req.json()
-    if(!sessionId) return NextResponse.json({message:'success',redirect:'/dashboard'})
+    if(!sessionId) return NextResponse.json({message:'success',redirect:'/dashboard',error:"noSessionId"})
 
-    const user=currentUser()
+    const user=await currentUser()
+    
     const sessionData=await db.select().from(SessionData).where(eq(SessionData.sessionId,sessionId))
-    if(sessionData.length===0) return NextResponse.json({message:'success',redirect:'/dashboard'})
+    if(sessionData.length===0) return NextResponse.json({message:'success',redirect:'/dashboard',error:'no sessionData'})
    const {assessmentId,versionId}=await generateAssessmentWithSessionData(sessionData,user,sessionId)
 return NextResponse.json({message:'success',redirect:`/assessments/${assessmentId}/${versionId}/edit/tests`})
-}catch(e){
+}catch(e:any){
+    console.log(e.message);
+    
     return NextResponse.json({message:'success',redirect:'/dashboard'})
 }
 
